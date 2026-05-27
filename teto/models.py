024529cf620600e1fc42b8e11d9bcf484a225847
lupin0960 <totalbattlewjsdyd@gmail.com>
2026-05-27 10:17:22 +0900

    refactor: migrate models.py to models/ package

--- teto/models.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Dict, Any
-
-
-class BaseModel(ABC):
-    @classmethod
-    @abstractmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "BaseModel":
-        pass
